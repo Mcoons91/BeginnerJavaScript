cd9e28063794a701cdd6e914d71fd198acfb4e74
Melissa Foster <34318301+Mcoons91@users.noreply.github.com>
2023-10-25 16:57:03 -0400

    Update arithmeticPractice.js
    corrected parenthesis in formula.

--- arithmeticPractice.js
+++ arithmeticPractice.js
@@ -1,6 +1,6 @@
 //formula to convert dog age to human age
 
 var dogAge = prompt("How old is your dog?");
-var humanAge = (dogAge-2*4+21);
+var humanAge = ((dogAge-2)*4)+21;
 
 alert("Your dog is " + humanAge + " years old in human years.");
