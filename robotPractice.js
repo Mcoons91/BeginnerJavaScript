c20b210bb9fb0fb0261473ecdee2f14e9d4cfb17
Melissa Foster <34318301+Mcoons91@users.noreply.github.com>
2023-11-10 13:21:15 -0500

    Add files via upload

---
+++ robotPractice.js
@@ -0,0 +1,29 @@
+//Learning to call functions by giving a pretend robot commands.
+//alert and console.log covered also
+
+function getMilk() {
+    alert("MoveUp")
+    alert("MoveDown")
+    alert("MoveLeft")
+    alert("MoveRight")
+    alert("MoveUp")
+    alert("MoveDown")
+    alert("MoveLeft")
+    alert("MoveRight")
+    alert("TakeABow")
+}
+
+function logMilk() {
+    console.log("MoveUp")
+    console.log("MoveDown")
+    console.log("MoveLeft")
+    console.log("MoveRight")
+    console.log("MoveUp")
+    console.log("MoveDown")
+    console.log("MoveLeft")
+    console.log("MoveRight")
+    console.log("TakeABow")
+}
+
+logMilk();
+getMilk();
